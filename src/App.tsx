b665d66601a5a5857975cb10192b270f9559ac99
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2024-07-09 23:14:05 -0700

    move camera to T spawn

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import * as React from 'react'
 import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { PointerLockControls, KeyboardControls } from '@react-three/drei'
 
 // gltfjsx generates a jsx file, ts compiler doesn't like it, i don't know, don't care.
 // @ts-ignore
@@ -10,7 +11,7 @@ import Dust2 from './Dust2'
 
 export default function App() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [-10, 3, 10], rotation: [0, -Math.PI/2, 0]}}>
       <Dust2 />
     </Canvas>
   )
